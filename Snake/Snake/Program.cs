b656a5c196eb56b99fa667c46d3ada90dde5d51d
trExie <slavagor.v@hotmail.com>
2016-03-23 04:46:21 +0300

    Линии

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,25 +16,11 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorizontalLine hline = new HorizontalLine(5, 10, 8, '+');
+            hline.Drow();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            VerticalLine vline = new VerticalLine(2, 4, 10, '#');
+            vline.Drow();
 
             Console.ReadLine();            
         }
